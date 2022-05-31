752e1ccda4129a825c5a4a09c448e4f57d612d6e
praveenprk <praveenkumarkp666@gmail.com>
2022-05-31 15:37:22 +0530

    removed helper functions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-    helper_method :current_user, :current_user_groups, :my_posts, :all_posts, :get_name
+    helper_method :current_user, :current_user_groups, :my_posts, :get_name
     
     def current_user
         if session[:user_id]
@@ -12,16 +12,6 @@ class ApplicationController < ActionController::Base
         if current_user
             Group.where(user_id: current_user.id)
         end
-    end
-
-    # def my_posts
-    #     if current_user
-    #         Feed.where(user_id: current_user.id)
-    #     end
-    # end
-
-    def all_posts
-        Feed.all
     end
 
     def get_name(user_id)
