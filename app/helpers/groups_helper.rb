a7863ff19896edf7c0f30e79f96c9a02ac9d7871
praveenprk <praveenkumarkp666@gmail.com>
2022-05-31 15:46:24 +0530

    pushing the helper functions

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,9 @@
 module GroupsHelper
+    def get_group_membership_status
+        @group.group_members.where(user_id: current_user.id)[0].status
+    end
+
+    def get_user_info_invitations(user_id)
+        User.find(user_id)
+    end
 end
