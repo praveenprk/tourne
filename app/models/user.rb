19e1f367169812204b50a4980b130f0b52d9672f
praveenprk <praveenkumarkp666@gmail.com>
2022-05-31 15:43:25 +0530

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,17 @@ class User < ApplicationRecord
     validates :lastname, presence: true, length: {minimum: 3}
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true, length: {minimum: 6, maximum: 20}
+    enum sex: [:male, :female, :others]
     
     #create many groups
     has_many :groups
+    has_many :questions
+
     has_many :followers, class_name: "Friendship", foreign_key: "followed_id"
     has_many :following, class_name: "Friendship", foreign_key: "follower_id"
 
     #user can belong to many groups
-    belongs_to :group, optional: true
+    belongs_to :group_members, optional: true
 
     has_one :profile
     has_many :feeds
