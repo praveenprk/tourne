fb0d16dd020af5e4d6bc6aa5635918124231bc00
praveenprk <praveenkumarkp666@gmail.com>
2022-05-31 15:41:04 +0530

    Model for group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,8 @@
 class Group < ApplicationRecord
     #a group has many users
-    has_many :users
+    # has_one :user
+    has_many :group_members
+    has_one_attached :group_image
 
     # a group can at a time can be owned by one user
     belongs_to :user
