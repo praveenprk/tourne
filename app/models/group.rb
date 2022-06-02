b5b4fca2283eddfbeb92d3d702293e56d265c289
praveenprk <praveenkumarkp666@gmail.com>
2022-06-02 21:04:11 +0530

    group model with many posts

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,9 @@ class Group < ApplicationRecord
     #a group has many users
     # has_one :user
     has_many :group_members
+    has_many :group_posts
     has_one_attached :group_image
+
 
     # a group can at a time can be owned by one user
     belongs_to :user
