09a714088a6e7e501b81091cb6d549b6401e6d4f
praveenprk <praveenkumarkp666@gmail.com>
2022-06-02 21:06:19 +0530

    group posts controller

--- app/controllers/group_posts_controller.rb
+++ app/controllers/group_posts_controller.rb
@@ -1,2 +1,44 @@
 class GroupPostsController < ApplicationController
+    def index
+     @group_posts = GroupPost.where(group_id: params[:id])
+    end
+
+    def show
+    
+    end
+
+    def new
+        @group_post = GroupPost.new
+    end
+
+    def create
+        @group_post = GroupPost.new(group_post_params)
+        if @group_post.save!
+            flash[:alert] = "Group Post Saved"
+            redirect_to show_group_posts_path(params[:group_id])
+        else
+            flash[:alert] = "Group Post Saving Failed"
+        end
+    end
+
+    def edit
+    
+    end
+
+    def update
+    
+    end
+
+    def search
+
+    end
+
+    def destroy
+    
+    end
+
+    private
+    def group_post_params
+        params.require(:group_post).permit(:group_post, :current_location, :user_id, :group_id, :group_post_attachment)
+    end
 end
